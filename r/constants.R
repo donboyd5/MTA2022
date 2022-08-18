6e915fd596130555e24c90c5b0c82e79f5ffe70a
Don Boyd <donboyd5@gmail.com>
2022-08-18 17:44:24 -0400

    WIP

--- r/constants.R
+++ r/constants.R
@@ -24,7 +24,7 @@ constants$mtacos <- c("New York County", "Bronx", "Kings", "Queens", "Richmond",
             "Nassau", "Suffolk", "Orange", "Putnam", "Dutchess", "Westchester")
 constants$mtafips <- c(36005, 36027, 36047, 36059, 36061, 36071, 36079, 36081, 36085, 36087, 36103, 36119) |> as.character()
 
-constants$nycfips <- c(36005, 36047, 36061, 36081, 36085) |> as.character()7
+constants$nycfips <- c(36005, 36047, 36061, 36081, 36085) |> as.character()
 constants$nyccos <- c("New York County", "Bronx", "Kings", "Queens", "Richmond")
 
 
